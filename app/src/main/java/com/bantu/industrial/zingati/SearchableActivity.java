a92bec8b2a0a12184c722ccd9f3bba5a06967b57
Chaiwa Berian <chaiwa.berian@gmail.com>
2017-08-16 20:53:57 +0200

    Add Searchable Activity

---
+++ app/src/main/java/com/bantu/industrial/zingati/SearchableActivity.java
@@ -0,0 +1,14 @@
+package com.bantu.industrial.zingati;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ListViewCompat;
+
+public class SearchableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchable);
+    }
+}
